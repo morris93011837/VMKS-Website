243cd8a67c9b8d8ff866c363d1ef8c8f85a213e5
lorettayao <a0910330206@gmail.com>
2023-08-08 16:15:13 +0800

    management table

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,29 +1,69 @@
-import { useNavigate } from "react-router-dom"
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const daysOfWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  const scheduleData = [
+    ["9:00-12:00", "Brian", "Brian", "Brian", "Brian", "Brian", "Brian"],
+    ["13:00-16:00 (A)", "Brian", "Brian", "Brian", "Brian", "Brian", "Brian"],
+    ["13:00-16:00 (B)", "Brian", "Brian", "Brian", "Brian", "Brian", "Brian"],
+    ["18:00-21:00", "Brian", "Brian", "Brian", "Brian", "Brian", "Brian"],
+  ];
+
   return (
     <div>
       <div>Home Page</div>
       <p>
-        歡迎來到 Virtual MakerSpace的首頁
-        這裡空空如也
-        <br></br>
+        歡迎來到 Virtual MakerSpace 的首頁
+        這裡有東西囉
+        <br />
         應該要有:簡略公告、常用連結、管理員班表
       </p>
       <div>
         <b>Announcement</b>
-        <br></br>
+        <br />
         <button onClick={() => navigate("/AnnouncementPage")}>View all</button>
-        <br></br>
+        <br />
         <b>常用連結</b>
-        <br></br>
+        <br />
         <button>Edit</button>
-        <br></br>
+        <br />
         <div>
-          <b>班表</b>
+          <div style={{ textAlign: "center" }}>
+            <b>Opening Hours</b>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Time</th>
+                {daysOfWeek.map((day) => (
+                  <th key={day}>{day}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {scheduleData.map((shifts, index) => (
+                <tr key={index}>
+                  {shifts.map((shift, shiftIndex) => (
+                    <td key={shiftIndex}>{shift}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
