08819d0a9ded8345af980df8f0e3b1cea2d4c6ac
lorettayao <a0910330206@gmail.com>
2023-08-13 13:57:54 +0800

    Footer done

--- frontend/src/component/Footer.tsx
+++ frontend/src/component/Footer.tsx
@@ -3,12 +3,39 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-      <div>
-        VMKS
-      </div>
+      <img
+        src="VMKS_logo.png"
+        alt="Footer Image"
+        style={{ width: '100px', height: '100px' , float:'left',marginRight:'100px'}} 
+      />
+      <p style={{ fontSize: '30px', color: '#333', float:'left' }}>Virtual MKS</p>
       
-    </footer>
-    
+      <a href="https://www.facebook.com/ntuee.makerspace/?locale=zh_TW">
+      <img
+          src={"https://cdn-icons-png.flaticon.com/512/4628/4628653.png"}
+          alt="FB link"
+          style={{
+            width: '100px',
+            height: '100px',
+            /*float: 'left',
+            marginRight: '10px',*/
+          }}
+        />
+      </a>
+      <a href="https://www.instagram.com/ntu.taiwan/">
+      <img
+          src={"https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png"}
+          alt="FB link"
+          style={{
+            width: '100px',
+            height: '100px',
+            /*float: 'left',
+            marginRight: '10px',*/
+          }}
+        />
+      </a>
+      </footer>
+
   )
 }
 
