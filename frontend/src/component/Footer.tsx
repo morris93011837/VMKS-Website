f99d71589adcd0c6248eb671a392296a33b9400a
謝沅瓏 <hsiehyuanlung@xieyuanlongde-MacBook-Pro.local>
2023-08-13 17:14:47 +0800

    EDITOR-#106 change footer img size

--- frontend/src/component/Footer.tsx
+++ frontend/src/component/Footer.tsx
@@ -6,34 +6,34 @@ const Footer = () => {
       <img
         src={vmks_footer_logo}
         alt="Footer"
-        style={{ width: '100px', height: '100px' , float:'left',marginRight:'100px'}} 
+        style={{ width: '100px', height: '100px', float: 'left', marginRight: '100px' }}
       />
-      
+
       <a href="https://www.facebook.com/ntuee.makerspace/?locale=zh_TW">
-      <img
+        <img
           src={"https://cdn-icons-png.flaticon.com/512/4628/4628653.png"}
           alt="FB link"
           style={{
-            width: '100px',
-            height: '100px',
+            width: '50px',
+            height: '50px',
             /*float: 'left',
             marginRight: '10px',*/
           }}
         />
       </a>
       <a href="https://www.instagram.com/ntu.taiwan/">
-      <img
+        <img
           src={"https://cdn.icon-icons.com/icons2/2066/PNG/512/instagram_icon_125245.png"}
           alt="FB link"
           style={{
-            width: '100px',
-            height: '100px',
+            width: '50px',
+            height: '50px',
             /*float: 'left',
             marginRight: '10px',*/
           }}
         />
       </a>
-      </footer>
+    </footer>
 
   )
 }
